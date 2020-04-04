e639176778b6be9773f39320dbc62089ea57d182
AmmarKhalid123 <ma05168@st.habib.edu.pk>
2020-04-04 11:20:42 +0500

    Assgn-1

---
+++ src/components/dishDetailComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+    renderDish(dish){
+            return (
+                <Card>
+                    <CardImg top width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        
+    }
+
+    renderComments(comments){
+        if (comments != null) {
+          const commentList = comments.map((commentObj) => {
+              return (
+              <div key={comments.id}>
+                  <p>{commentObj.comment}</p>
+                  <p>-- {commentObj.author} {commentObj.date}</p>
+              </div>);
+          }
+          );
+          return  (<div>
+                <h4>
+                    Comments
+                </h4>
+                <div className="list-unstyled">
+                    {commentList}
+                </div>
+            </div>
+        );}
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.selectedDish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish.comments)}
+                </div>
+            </div>
+        );
+        
+}
+}
+
+
+export default DishDetail;
--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './dishDetailComponent';
+
 
 class Menu extends Component {
     constructor (props) {
@@ -15,15 +17,9 @@ class Menu extends Component {
     renderDish(dish){
         if (dish != null){
             return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                       <CardImgOverlay>
-                       </CardImgOverlay>
-                </Card>
+                <div>
+                <DishDetail selectedDish={dish}/>
+                </div>
             );
         }
         else{
@@ -51,7 +47,7 @@ class Menu extends Component {
                 <div className="row">
                         {menu}
                 </div>
-                <div className="row">
+                <div>
                     {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
