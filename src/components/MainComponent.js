168ea043554bb335f9bcd8eed9aca7afa20da117
AmmarKhalid123 <ma05168@st.habib.edu.pk>
2020-04-11 11:27:34 +0500

    SPA Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './menuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './dishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect} from 'react-router-dom';
@@ -13,13 +17,18 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion = {this.state.promotions.filter((promotion) => promotion.featured)[0]}
+        leader = {this.state.leaders.filter((leader) => leader.featured)[0]}/>
       );
     }
 
@@ -29,8 +38,10 @@ class Main extends Component {
       <Switch>
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+        <Route exact path="/contactus" component={Contact}/>
         <Redirect to="/home" />
       </Switch>
+
       <Footer/>
     </div>
   );
